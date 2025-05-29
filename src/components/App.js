1dc0b53bc92787421ce32034a84b3f37139575dc
Sahebzad Kushal Deep <kushaldeep448@gmail.com>
2025-05-29 16:55:13 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,58 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name,setName]=useState("")
+  const [price,setPrice]=useState("")
+  const [result,setResult]=useState([])
+
+  const addItem = ()=>{
+    setResult([...result,`${name}-${price}`])
+    setName("")
+    setPrice("");
+
+  }
+
+  const Remove=(indextoremove)=>{
+   const updatedresult= result.filter((_, index) => index !== indextoremove);
+   setResult(updatedresult)
+  }
+
   return (
-    <div>
+    <div className="parent">
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        <label>Item name: <input type="text"  value={name} id="itemName" onChange={e=>setName(e.target.value)}></input></label>
+        <label>Item price<input type="text" value={price}  id="itemPrice" onChange={e=>setPrice(e.target.value)}></input></label>
+        
+        <button onClick={addItem}>Add Item</button>
+        <Child result={result} Remove={Remove}></Child>
     </div>
   )
 }
 
-export default App
+
+const Child =({result,Remove}) =>{
+
+  
+
+  
+  return(
+    <div className="child">
+      <h2>Child Component</h2>
+      <ul>
+        {
+          result.map((item,index)=>(
+            <li key={index}>{item}
+            <button onClick={()=>Remove(index)}>Remove</button>
+            </li>
+
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App;
